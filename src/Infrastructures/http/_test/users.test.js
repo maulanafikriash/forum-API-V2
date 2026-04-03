58110888bd910d2e196ae43e857b106f901a283d
maulanafikriash <maulanx835@gmail.com>
2026-04-03 22:42:40 +0700

    feat: add hello everyone test case

--- src/Infrastructures/http/_test/users.test.js
+++ src/Infrastructures/http/_test/users.test.js
@@ -138,4 +138,16 @@ describe('/users endpoint', () => {
       expect(response.body.data).toEqual('Hello world!');
     });
   });
+  // Test Case 2
+  describe('when GET /everyone', () => {
+    it('should return 200 and hello world', async () => {
+    // Arrange
+      const app = await createServer({});
+      // Action
+      const response = await request(app).get('/everyone');
+      // Assert
+      expect(response.status).toEqual(200);
+      expect(response.body.data).toEqual('Hello everyone!');
+    });
+  });
 });
